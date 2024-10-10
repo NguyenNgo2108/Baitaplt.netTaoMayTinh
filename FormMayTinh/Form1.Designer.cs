ef31eae545d72e09b62a789eb5ffd5278d9351e0
NguyenNgo2108 <nguyenngo2102@gmail.com>
2024-10-10 15:18:31 +0700

    baitap

---
+++ FormMayTinh/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace FormMayTinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbKetQua = new TextBox();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btnCong = new Button();
+            btnTru = new Button();
+            buttonNhan = new Button();
+            btn0 = new Button();
+            btnChia = new Button();
+            btnXoa = new Button();
+            btnTinh = new Button();
+            SuspendLayout();
+            // 
+            // tbKetQua
+            // 
+            tbKetQua.Location = new Point(45, 32);
+            tbKetQua.MaxLength = 300;
+            tbKetQua.Name = "tbKetQua";
+            tbKetQua.Size = new Size(589, 27);
+            tbKetQua.TabIndex = 0;
+            tbKetQua.TextChanged += tbKetQua_TextChanged;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(45, 91);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(94, 64);
+            btn1.TabIndex = 1;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(195, 91);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(94, 64);
+            btn2.TabIndex = 2;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(348, 91);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(94, 64);
+            btn3.TabIndex = 3;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(45, 189);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(94, 64);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(195, 189);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(94, 64);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(348, 189);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(94, 64);
+            btn6.TabIndex = 6;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(45, 293);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(94, 64);
+            btn7.TabIndex = 7;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(195, 293);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(94, 64);
+            btn8.TabIndex = 8;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(348, 293);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(94, 64);
+            btn9.TabIndex = 9;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btnCong
+            // 
+            btnCong.Location = new Point(540, 91);
+            btnCong.Name = "btnCong";
+            btnCong.Size = new Size(94, 64);
+            btnCong.TabIndex = 10;
+            btnCong.Text = "+";
+            btnCong.UseVisualStyleBackColor = true;
+            btnCong.Click += btnCong_Click;
+            // 
+            // btnTru
+            // 
+            btnTru.Location = new Point(540, 189);
+            btnTru.Name = "btnTru";
+            btnTru.Size = new Size(94, 64);
+            btnTru.TabIndex = 11;
+            btnTru.Text = "-";
+            btnTru.UseVisualStyleBackColor = true;
+            btnTru.Click += btnTru_Click;
+            // 
+            // buttonNhan
+            // 
+            buttonNhan.Location = new Point(540, 293);
+            buttonNhan.Name = "buttonNhan";
+            buttonNhan.Size = new Size(94, 64);
+            buttonNhan.TabIndex = 12;
+            buttonNhan.Text = "*";
+            buttonNhan.UseVisualStyleBackColor = true;
+            buttonNhan.Click += buttonNhan_Click;
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(195, 384);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(94, 64);
+            btn0.TabIndex = 13;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btnChia
+            // 
+            btnChia.Location = new Point(540, 384);
+            btnChia.Name = "btnChia";
+            btnChia.Size = new Size(94, 64);
+            btnChia.TabIndex = 14;
+            btnChia.Text = "/";
+            btnChia.UseVisualStyleBackColor = true;
+            btnChia.Click += btnChia_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(45, 384);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 64);
+            btnXoa.TabIndex = 15;
+            btnXoa.Text = "C";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnTinh
+            // 
+            btnTinh.Location = new Point(348, 384);
+            btnTinh.Name = "btnTinh";
+            btnTinh.Size = new Size(94, 64);
+            btnTinh.TabIndex = 16;
+            btnTinh.Text = "=";
+            btnTinh.UseVisualStyleBackColor = true;
+            btnTinh.Click += btnTinh_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnTinh);
+            Controls.Add(btnXoa);
+            Controls.Add(btnChia);
+            Controls.Add(btn0);
+            Controls.Add(buttonNhan);
+            Controls.Add(btnTru);
+            Controls.Add(btnCong);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(tbKetQua);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbKetQua;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btnCong;
+        private Button btnTru;
+        private Button buttonNhan;
+        private Button btn0;
+        private Button btnChia;
+        private Button btnXoa;
+        private Button btnTinh;
+    }
+}
---
+++ FormMayTinh/Form1.cs
@@ -0,0 +1,150 @@
+﻿namespace FormMayTinh
+{
+    public partial class Form1 : Form
+    {
+        private double result = 0;
+        private string operation = "";
+        private bool operationPressed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbKetQua_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AppendNumber(string number)
+        {
+            if (operationPressed)
+            {
+                tbKetQua.Text = number;
+                operationPressed = false;
+            }
+            else
+            {
+                tbKetQua.Text += number;
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            AppendNumber("1");
+        }
+
+        private void btn2_Click(object sender, EventArgs e) 
+        {
+            AppendNumber("2");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            AppendNumber("3");
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            AppendNumber("4");
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            AppendNumber("5");
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            AppendNumber("6");
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            AppendNumber("7");
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            AppendNumber("8");
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            AppendNumber("9");
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            AppendNumber("0");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            tbKetQua.Clear();
+            result = 0;
+            operation = "";
+        }
+
+        private void SetOperation(string op)
+        {
+            if (result != 0)
+            {
+                btnTinh.PerformClick();
+                operationPressed = true;
+            }
+            else
+            {
+                result = double.Parse(tbKetQua.Text);
+                operation = op;
+                operationPressed = true;
+            }
+        }
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            if (operation != "")
+            {
+                switch (operation)
+                {
+                    case "+":
+                        tbKetQua.Text = (result + double.Parse(tbKetQua.Text)).ToString();
+                        break;
+                    case "-":
+                        tbKetQua.Text = (result - double.Parse(tbKetQua.Text)).ToString();
+                        break;
+                    case "*":
+                        tbKetQua.Text = (result * double.Parse(tbKetQua.Text)).ToString();
+                        break;
+                    case "/":
+                        if (double.Parse(tbKetQua.Text) != 0)
+                            tbKetQua.Text = (result / double.Parse(tbKetQua.Text)).ToString();
+                        else
+                            MessageBox.Show("Không thể chia cho 0!");
+                        break;
+                }
+                result = double.Parse(tbKetQua.Text);
+                operation = "";
+
+            }
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            SetOperation("+");
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            SetOperation("-");
+        }
+
+        private void buttonNhan_Click(object sender, EventArgs e)
+        {
+            SetOperation("*");
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            SetOperation("/");
+        }
+    }
+}
